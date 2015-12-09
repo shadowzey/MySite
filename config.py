0fbf78b20a02d565157e66c7ac1eb2f128767335
yanze <yanze000@hotmail.com>
2015-12-09 15:39:35 +0800

    将mysql移到docker

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    
+
     @staticmethod
     def init_app(app):
         pass
@@ -16,9 +16,8 @@ class DevelopmentConfig(Config):
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://root:12342234yanze@localhost/moviedb'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://admin:12342234yanze@174.1.40.2:3306/moviedb'
 
 config = {
     'development': DevelopmentConfig,
     }
-
